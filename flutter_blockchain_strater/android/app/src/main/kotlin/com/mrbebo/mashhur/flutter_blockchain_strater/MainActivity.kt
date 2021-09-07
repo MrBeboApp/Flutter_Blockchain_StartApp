9fe9ea20b566860c1daac9de5c0478748a44caac
Bahaa Ehab El Dien Abdelmawla <mbcompany888@gmail.com>
2021-09-07 16:52:41 +0200

    Flutter intial

---
+++ flutter_blockchain_strater/android/app/src/main/kotlin/com/mrbebo/mashhur/flutter_blockchain_strater/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mrbebo.mashhur.flutter_blockchain_strater
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_blockchain_strater/lib/pages/homePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage() : super();
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/ardapps.png",scale: .8,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Welcome From arDapps.com",style:TextStyle(fontSize: 20),),
+              ),
+            ],
+          )),
+      ),
+    );
+  }
+}
